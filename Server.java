960f1ea0e4494ea4a8e1ac3ff8495003b9574d3d
petterz <petter.zethelius@gmail.com>
2017-11-22 16:45:36 +0100

    Add files via upload
    hueuehueueueuheu javadoc

--- Server.java
+++ Server.java
@@ -2,10 +2,20 @@ import java.util.*;
 import java.io.*;
 import java.net.*;
 
+
+/**
+ * class for the server part of the part of the twitterish program that allows clients to
+ *communicate with each other. 
+ */
 public class Server {
     private Set<Account> knownUsers = new TreeSet<Account>();
     private List<Post> posts = new LinkedList<Post>();
 
+
+    /**
+     * main function to initiate the server
+     */
+    
     public static void main(String[] args) {
         try {
             ServerSocket socket = new ServerSocket(args.length > 0 ? Integer.parseInt(args[0]) : 8080);
@@ -28,6 +38,13 @@ public class Server {
         }
     }
 
+    
+    /**
+     * Allows you to access an account by using it's userId
+     *@param userId the userId for an account
+     *@return Account if an Account matches the given userId, otherwise if no account is found it returns null
+     */
+    
     public Account getAccountFor(String userId) {
         for (Account a : this.knownUsers)
             if (a.getUserId().equals(userId)) return a;
@@ -35,25 +52,62 @@ public class Server {
         return null;
     }
 
+    
+    /**
+     * Adds a given account to knownUsers
+     * @param a is an account
+     */
+    
     public synchronized void addAccount(Account a) {
         this.knownUsers.add(a);
     }
 
+    
+    /**
+   *removes an account from knownUsers
+   *@param a is an account
+   */
+    
     public synchronized void removeAccount(Account a) {
         this.knownUsers.remove(a);
     }
 
+    
+    /**
+     *Gives the requester all accounts from the server
+     *@return Set<Account> a set of all accounts in knownUsers
+     */
+    
     public synchronized Set<Account> getAccounts() {
         return new TreeSet<Account>(this.knownUsers);
     }
 
+    
+    /**
+     * gives the requester all posts from the server
+     * @return List<Post> a list of all posts
+     */
+    
     public synchronized List<Post> getPosts() {
         return new ArrayList<Post>(this.posts);
     }
+    
 
+    /**
+     * Adds a post to posts
+     * @param p is a Post
+     */
+    
     public synchronized void addPost(Post p) {
         this.posts.add(p);
     }
+
+
+    /**
+     * gets all the posts from the requesting users friends.
+     *@param a is an Account
+     *@return List<Post> a list of post
+     */
     
     public synchronized List<Post> getNewFriendPosts(Account a)
     {
@@ -66,22 +120,20 @@ public class Server {
         
         return result;
         }
-    
+    /**
+     * gives back all posts that have been posted since last sync
+     *@param a is an Account
+     *@return List<Post> a list of posts
+     */
     public synchronized List<Post> getNewPosts(Account a){
         int postsLastSync=a.getPostAtLastSync();
         a.setPostAtLastSync(this.posts.size());
         return new ArrayList<Post>(this.posts.subList(postsLastSync, this.posts.size()));
     }
 
-    /*   public void changePosterName(String oldName, String newName) {
-        for (Account a:knownUsers) {
-            if (a.getName()==oldName) {
-                a.setName(newName);
-                }
-            }
-
-        }
-    */
+    /**
+     * a subclass of server that holding methods to communicate with clients
+     */
     
     static class ClientProxy extends Thread {
         private Account account;
@@ -101,6 +153,15 @@ public class Server {
             this.outgoing.flush();
         }
 
+
+
+        /**
+         *attempts to establish connection between the respective client and the server, with the
+         * details from the socket, checks if the user trying to connect already exists.  
+         *@param server is the server.
+         *@param socket is a Socket, that contains details such as server ip and portnumber
+         */ 
+        
         public static void attemptEstablishConnection(Socket socket, Server server) throws IOException, ClassNotFoundException {
             ObjectInputStream incoming = new ObjectInputStream(socket.getInputStream());
             Object handShake = incoming.readObject();
@@ -120,15 +181,23 @@ public class Server {
                 System.err.println("!! Bad connection attempt from: " + socket.getInetAddress() + ":" + socket.getPort());
             }
         }
-
         private int globalPostIdCounter = 0;
-        // The synchronised keyword is required on all methods which may
-        // be called in parallel on the server from multiple clients at
-        // the same time.
+        
+        /**
+         *gives each post an individual unique id. 
+         *@return the unique id of the post
+         */
+        
         private synchronized int getUniqueGlobalPostId() {
             return ++this.globalPostIdCounter;
         }
 
+
+        /**
+         * logs the user off from the server
+         * @param a is an Account
+         */
+        
         private void logout(Account a) {
             this.server.removeAccount(a);
             System.out.println("!! " + a.getUserId() + " left the building");
@@ -140,31 +209,55 @@ public class Server {
             }
         }
 
+
+        /**
+         * allows the user to post a new message to the server
+         *@param msg is a string, and is the message to be posted to the server
+         */
+        
         private void postMessage(String msg) {
             this.server.addPost(new Post(this.getUniqueGlobalPostId(), this.account, msg));
         }
 
+
+        /**
+         * adds a friend to the users friendslist
+         * @param a is the account to be added to the users friendslist
+         */
+        
         private void addFriend(Account a) {
             this.account.addFriend(a);
             a.addFriend(this.account);
         }
 
+
+
+        /**
+         * removes a friend from the accounts friendslist
+         * @param a is the friend to be removed
+         */
+        
         private void removeFriend(Account a) {
             this.account.removeFriend(a);
             a.removeFriend(this.account);
         }
 
+        
+        /**
+         *Allows the user the change it's name
+         *@param old is the the current user
+         *@param neu is what the account should be updated to
+         */
+        
         private void updateAccount(Account old, Account neu) {
-            // for (Post p: this.server.posts) {
-                //     if ((p.getPoster()).getUserId()==neu.getUserId())
-                //  p.setPoster(neu);
-                // }
             Account A= server.getAccountFor(old.getUserId());
-            A.setName(neu.getName());
-            //      server.removeAccount(old);
-            // server.addAccount(neu);
-            
+            A.setName(neu.getName()); 
         }
+
+
+        /**
+         * send a synqrequest to the the client with the post list and account list
+         */
 
         private void sync() {  
             try {
@@ -177,7 +270,14 @@ public class Server {
                 ioe.printStackTrace();
             }      
             }
-        
+
+
+
+        /**
+         * is an infinte loop that waits for an incomming message with an object, depending
+         * on what type of object different functions will be applied to it.
+         *
+         */
         public void run() {
             try {
                 while (true) {
