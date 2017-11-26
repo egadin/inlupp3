9e5ba79d74abddcef423928e50d53146411ec912
egadin <godda96@gmail.com>
2017-11-26 17:34:17 +0100

    javaDoc kommentarer klart

--- Message.java
+++ Message.java
@@ -1,2 +1,6 @@
+/**
+ * Class for the structure of a message between the server and the client
+ */
+
 public class Message implements java.io.Serializable {
 }
--- SyncRequest.java
+++ SyncRequest.java
@@ -1,2 +1,6 @@
+/**
+ * Class for the message of type sync request between the server and client
+ */
+
 public class SyncRequest implements java.io.Serializable {
 }
